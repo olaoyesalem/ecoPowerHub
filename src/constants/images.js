6124e182f0dfd0c59ce376b6857fc4fa7ddf095a
Johnson <ogbonnayajprof2015@gmail.com>
2023-11-14 15:57:05 +0100

    add images

--- src/constants/images.js
+++ src/constants/images.js
@@ -7,6 +7,46 @@ import ppp1 from "../assets/ppp1.png";
 import spp from "../assets/spp.png";
 import spp1 from "../assets/spp1.png";
 import pp1 from "../assets/pp1.png";
-import pp2 from "../assets/pp2.png";
+import dog2 from "../assets/dog2.png";
+import girl from "../assets/girl.png";
+import monkey from "../assets/monkey.png";
+import robot from "../assets/robot.png";
+import robot2 from "../assets/robot2.png";
+import space from "../assets/space.png";
+import space2 from "../assets/space2.png";
+import space3 from "../assets/space3.png";
+import space4 from "../assets/space4.png";
+import space5 from "../assets/space5.png";
+import dog from "../assets/dog.png";
+import cloud from "../assets/cloud.png";
+import cat from "../assets/cat.png";
+import cat2 from "../assets/cat2.png";
+import catman from "../assets/catman.png";
 
-export default { heroImg, a1, icon, img, ppp, ppp1, spp, spp1,pp1,pp2 };
+export default {
+  heroImg,
+  a1,
+  icon,
+  img,
+  ppp,
+  ppp1,
+  spp,
+  spp1,
+  pp1,
+  pp2,
+  dog2,
+  girl,
+  monkey,
+  robot,
+  robot2,
+  space,
+  space2,
+  space3,
+  space4,
+  space5,
+  dog,
+  cloud,
+  cat,
+  cat2,
+  catman,
+};
