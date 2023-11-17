586a64790c269e95ce32d195540f189d33264f46
Johnson <ogbonnayajprof2015@gmail.com>
2023-11-18 00:08:49 +0100

    fixed hamburger menu

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,10 +2,17 @@ import Button from "../Button/Button";
 import { FaShop } from "react-icons/fa6";
 import { GoPerson } from "react-icons/go";
 import { FaBarsStaggered } from "react-icons/fa6";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const onMenuClick = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="mx-auto my-4 flex justify-between items-center w-11/12">
+    <nav className="relative mx-auto my-4 flex justify-between items-center w-11/12">
       <div id="logoCont" className="flex items-center space-x-2">
         <span className="text-ctaColor">
           <FaShop />
@@ -14,15 +21,52 @@ const NavBar = () => {
           Energy Marketplace
         </a>
       </div>
-      <button className="lg:hidden">
-        <FaBarsStaggered />
+      <button
+        id="menu-btn"
+        class={`block hamburger lg:hidden focus:outline-none ${
+          isMenuOpen === false ? "" : "open"
+        }`}
+        onClick={onMenuClick}
+      >
+        <span class="hamburger-top"></span>
+        <span class="hamburger-middle"></span>
+        <span class="hamburger-bottom"></span>
       </button>
-
-      <div className="hidden justify-between space-x-4 items-center w-1/2 lg:flex md:text-sm xxl:w-1/2 lg:text-base md:w-3/5 lg:w-3/5">
-        {/* <div className="flex "> */}
-        <a href="marketplace">Marketplace</a>
-        <a href="#">Rankings</a>
-        <a href="connect-wallet">Connect a wallet</a>
+      <div
+        id="menu"
+        class={`absolute flex flex-col items-center self-end py-6 top-14 space-y-4 font-bold bg-black text-darkGrey sm:w-auto sm:self-center left-6 right-6 drop-shadow-md ${
+          isMenuOpen === false ? "" : "flex"
+        } ${isMenuOpen === false ? "hidden" : ""}`}
+      >
+        <a
+          href="marketplace"
+          className="font1 p-3 rounded-full hover:border border-ctaColor"
+        >
+          Marketplace
+        </a>
+        <a
+          href="#"
+          className="font1 p-3 rounded-full hover:border border-ctaColor"
+        >
+          Rankings
+        </a>
+        <a
+          href="connect-wallet"
+          className="font1 p-3 rounded-full hover:border border-ctaColor"
+        >
+          Connect a wallet
+        </a>
+      </div>
+      <div className="hidden justify-center justify-between items-center w-1/2  lg:space-x-4 md:text-sm xxl:w-1/2 lg:text-base md:w-3/5 lg:w-3/5 lg:flex">
+        <a href="marketplace" className="font1">
+          Marketplace
+        </a>
+        <a href="#" className="font1">
+          Rankings
+        </a>
+        <a href="connect-wallet" className="font1">
+          Connect a wallet
+        </a>
         <Button
           className="py-4 px-5 bg-ctaColor rounded-2xl flex items-center space-x-2"
           href="#"
@@ -30,7 +74,6 @@ const NavBar = () => {
           <GoPerson />
           <span>Sign Up</span>
         </Button>
-        {/* </div> */}
       </div>
     </nav>
   );
